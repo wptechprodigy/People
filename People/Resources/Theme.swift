40647b55ac108c26a94925bd994dbdd949035a56
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-10-20 17:40:00 +0100

    Create color set for project components

---
+++ People/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  People
+//
+//  Created by waheedCodes on 20/10/2022.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("Background")
+    static let detailBackground = Color("Detail_Background")
+    static let text = Color("Text")
+    static let ribbon = Color("Ribbon")
+}
