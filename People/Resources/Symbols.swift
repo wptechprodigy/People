675ddb6ac0737177778bcc59711821e66e524eea
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-10-20 17:26:59 +0100

    Initial project setup

---
+++ People/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  People
+//
+//  Created by waheedCodes on 20/10/2022.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.fill")
+    static let plus = Image(systemName: "plus")
+    static let settings = Image(systemName: "gear")
+}
