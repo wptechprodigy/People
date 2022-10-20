0d969971a6b7df90a1e1c444fd9fd64583594f35
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-10-20 17:13:18 +0100

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by waheedCodes on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
